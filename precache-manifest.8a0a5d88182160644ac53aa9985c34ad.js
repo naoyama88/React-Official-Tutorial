745c34dfddd227eb6602c744d389a39fe29225ce
naoyama88 <naoyama88@gmail.com>
2019-10-16 03:07:39 -0700

    Updates

---
+++ precache-manifest.8a0a5d88182160644ac53aa9985c34ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f594493bd1b1bd45c0192ee270c8ff0c",
+    "url": "/React-Official-Tutorial/index.html"
+  },
+  {
+    "revision": "b45d248013665b6b51cd",
+    "url": "/React-Official-Tutorial/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "355dffa62f6f027c815d",
+    "url": "/React-Official-Tutorial/static/js/2.aa4d1464.chunk.js"
+  },
+  {
+    "revision": "b45d248013665b6b51cd",
+    "url": "/React-Official-Tutorial/static/js/main.4b72eb0a.chunk.js"
+  },
+  {
+    "revision": "bba17b84e8d2c2cc7380",
+    "url": "/React-Official-Tutorial/static/js/runtime-main.e18ddaba.js"
+  }
+]);
